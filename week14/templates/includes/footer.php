b9ec01c3e63ac5c5f3e45b7202e4ac96ced73b8c
Maxim Safioulline <maxim@connectedcatmedia.com>
2021-06-14 14:13:54 -0700

    Added PHP-based templates to week 14

---
+++ week14/templates/includes/footer.php
@@ -0,0 +1,11 @@
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+            <span class="text-muted">Copyright SMC WebDesign3 class, spring 2021 &copy;.</span>
+        </div>
+    </footer>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+  </body>
+</html>
